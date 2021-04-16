d76eb0dd1b1202741cde72ebf1ab8d2cfb611f81
putri810 <76274733+putri810@users.noreply.github.com>
2021-04-16 20:00:02 +0700

    Add files via upload

---
+++ PackageTest/PackageTest.java
@@ -0,0 +1,16 @@
+package PackageTest;
+
+import com.horstmann.corejava.*;
+
+import static java.lang.System.*;
+public class PackageTest
+{
+	public static void ain(String[] args)
+	{
+	Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+	
+	harry.raiseSalary(5);
+	
+	out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
+	}
+}
